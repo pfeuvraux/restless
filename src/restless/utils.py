96b2596881e307c7b44e7bdfe02f2ae6add91bdc
Paul Feuvraux <pfeuvraux@gmail.com>
2021-04-07 22:37:16 +0200

    added utils

--- src/restless/utils.py
+++ src/restless/utils.py
@@ -1,6 +1,7 @@
 from os import urandom
 from json import dumps
 from typing import Any
+import base64
 
 def rand_gen(size: int = 64) -> bytes:
   return urandom(size)
@@ -8,5 +9,13 @@ def rand_gen(size: int = 64) -> bytes:
 def tojson(data: Any):
   return dumps(obj=data, indent=2)
 
-def str_to_bytes(data: str, encoding: str = "utf-8"):
-  return str.encode(encoding)
+def str_to_bytes(data: str) -> bytes:
+  return data.encode()
+
+def bytes_to_str(data: bytes) -> str:
+  return data.decode()
+
+def bytes_to_base64(data: bytes) -> str:
+    return bytes_to_str(
+      data=base64.encode(data)
+    )
